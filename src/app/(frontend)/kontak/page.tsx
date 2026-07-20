066cecd9d6f2bb74478004f373992cab7646f484
Dicoment Agency <github@dicoment.com>
2026-07-20 14:39:51 +0700

    sama

--- src/app/(frontend)/kontak/page.tsx
+++ src/app/(frontend)/kontak/page.tsx
@@ -1,6 +1,8 @@
 import FaqSection from "@/components/sections/home/FaqSection"
 import ContactClient from "./ContactClient"
 
+export const dynamic = 'force-dynamic';
+
 export default function KontakPage() {
   return (
     <>
